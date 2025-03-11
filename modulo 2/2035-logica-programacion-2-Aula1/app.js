25e88924352b34ae98a39436c4531dbc3b2c6b70
Valentina Peralta <alexandra.peralta.g@uni.pe>
2025-03-11 00:20:52 -0500

    Elimina todo el contenido del repositorio

--- modulo 2/2035-logica-programacion-2-Aula1/app.js
+++
@@ -1,9 +0,0 @@
-let titulo = document.querySelector('h1')
-titulo.innerHTML = 'Juego del número secreto';
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10';
-
-function intentoDeUsuario(){
-    alert('Click desde el boton');
-}
